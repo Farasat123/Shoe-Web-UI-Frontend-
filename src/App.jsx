33b2c4baab07cb387f9b1de481c1c72e6fd30d6e
Farasat123 <abbasfarasat0786@gmail.com>
2026-05-01 13:34:03 +0500

    Initial commit: Nike shoe React app

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import Thaiwland from 'tailwindcss';
+export  function App() {
+  return (
+    <div className="h-screen flex items-center justify-center">
+      <h1 className="text-5xl font-bold text-red-500">Tailwind Works 🚀</h1>
+    </div>
+  );
+}
+
+export default App;
